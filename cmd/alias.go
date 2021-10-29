d3a51c6a1b55942cb6177180e3c392954ab01ec0
Souvik Kar Mahapatra <souvikat001@gmail.com>
2021-10-29 12:44:44 +0530

    list alias command

---
+++ cmd/alias.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/gookit/color"
+	"github.com/souvikinator/lsx/utils"
+	"github.com/spf13/cobra"
+)
+
+var aliasCmd = &cobra.Command{
+	Use:   "alias",
+	Short: "lists all alias created by user",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if len(App.Alias) == 0 {
+			utils.Warn("no alias found in records")
+			utils.Warn("get started by using 'set-alias' command")
+			os.Exit(0)
+		}
+
+		for alias, p := range App.Alias {
+			color.Printf("* %s --> %s\n", color.Magenta.Render(alias), color.Yellow.Render(p))
+		}
+		os.Exit(0)
+
+	},
+}

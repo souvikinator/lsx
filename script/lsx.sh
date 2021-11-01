fb1743c9468115c9e225254164803fde80861f2a
Souvik Kar Mahapatra <souvikat001@gmail.com>
2021-11-02 02:16:43 +0530

    minor changes

--- script/lsx.sh
+++ script/lsx.sh
@@ -1,20 +1,19 @@
-TEMP_FILE="$HOME/.config/lsx/lsx.tmp"
-
-LSX_BANNER="
-    __    _  __
-   / /___| |/ /
-  / / ___/   / 
- / (__  )   |  
-/_/____/_/|_|tended
-
-github.com/souvikinator/lsx
-"
-
 lsx () {
+	TEMP_FILE="$HOME/.config/lsx/lsx.tmp"
+
+	LSX_BANNER="
+		 __    _  __
+		/ /___| |/ /
+	 / / ___/   / 
+	/ (__  )   |  
+ /_/____/_/|_|tended
+
+	github.com/souvikinator/lsx
+	"
+
 	ARGS=("$@")
 	ARGS_LEN="${#ARGS[@]}"
-
-
+	
 	if [[ ARGS_LEN -lt 1 ]];then
 		ls-x
 		LSX_CWD=$(cat "$TEMP_FILE")
@@ -22,7 +21,6 @@ lsx () {
 		true > "$TEMP_FILE"
 	else
 		if [[ "${ARGS[0]}" == "--help" ]];then
-			printf "%s\n" "$LSX_BANNER"
 			ls-x --help
 			return
 		else
